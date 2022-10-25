82b4f59111fa7dfc94f05f25de26eda6cf71cdb9
Pen <penelope_csw@outlook.com>
2022-10-25 21:22:16 +0200

    SVM model

--- SVM/SumCmap2SVM.py
+++ SVM/SumCmap2SVM.py
@@ -3,8 +3,8 @@
 import numpy as np
 import pandas as pd
 import os 
-import pandas as pd
-import seaborn as sns
+import argparse
+from joblib import dump, load
 from sklearn import svm
 from sklearn.svm import SVC, LinearSVC
 from sklearn.linear_model import SGDClassifier
@@ -17,46 +17,59 @@ from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.feature_selection import SelectKBest
 
-sum_cmap_dir = '../FeatureEngineering/SUM_contact_aa_maps_25/'
-directory = os.fsdecode(sum_cmap_dir)
-#print(directory)
+if __name__ == "__main__":
 
-# generate the dataset - read sum cmaps; read the label from AF_helix_sheet.tsv; 
-#                        combine to generate the dataset for model
+    parser = argparse.ArgumentParser(description='input cmap threshold for SVM training.')
+    parser.add_argument('cmap_threshold', help='an integer to select cmap folder for SVM training', type=int)
+    args = parser.parse_args()
 
-hf_pd = pd.read_csv('../Datasets/AF_helix_sheet.tsv', sep='\t', header=0)
-entry = hf_pd[["Entry"]]
-label = hf_pd[["label"]]
-entry_label_pd = pd.concat([entry, label], axis=1)
-entry_label_pd = entry_label_pd.set_index('Entry')
-entry_label_pd['SUM_CMap'] = ""
+    sum_cmap_dir = '../FeatureEngineering/SUM_contact_aa_maps_' + str(args.cmap_threshold) + '/'
+    directory = os.fsdecode(sum_cmap_dir)
+    #print(directory)
 
-for sum_cmap_file in os.listdir(sum_cmap_dir):
-    sum_cmap = pd.read_csv(sum_cmap_dir + sum_cmap_file, delim_whitespace=True, header=None)
-    #print(sum_cmap)
-    entry_name = sum_cmap_file[:-9]
-    entry_label_pd.loc[entry_name]['SUM_CMap'] = sum_cmap.values.flatten()
+    # generate the dataset - read sum cmaps; read the label from AF_helix_sheet.tsv; 
+    #                        combine to generate the dataset for model
 
-entry_label_sumcmap = entry_label_pd.loc[entry_label_pd['SUM_CMap']!=""]
+    hf_pd = pd.read_csv('../Datasets/AF_helix_sheet.tsv', sep='\t', header=0)
+    entry = hf_pd[["Entry"]]
+    label = hf_pd[["label"]]
+    entry_label_pd = pd.concat([entry, label], axis=1)
+    entry_label_pd = entry_label_pd.set_index('Entry')
+    entry_label_pd['SUM_CMap'] = ""
 
-# cross-validation for model performance evaluation
-pipe = make_pipeline(StandardScaler(), SVC(class_weight="balanced"))
-predicted = cross_val_predict(pipe, list(entry_label_sumcmap['SUM_CMap']), entry_label_sumcmap['label'], n_jobs=-1)
-print(classification_report(y_pred=predicted, y_true=entry_label_sumcmap['label']))
+    for sum_cmap_file in os.listdir(sum_cmap_dir):
+        sum_cmap = pd.read_csv(sum_cmap_dir + sum_cmap_file, delim_whitespace=True, header=None)
+        #print(sum_cmap)
+        entry_name = sum_cmap_file[:-9]
+        entry_label_pd.loc[entry_name]['SUM_CMap'] = sum_cmap.values.flatten()
+
+    entry_label_sumcmap = entry_label_pd.loc[entry_label_pd['SUM_CMap']!=""]
+
+    # cross-validation for model performance evaluation
+    pipe = make_pipeline(StandardScaler(), SVC(class_weight="balanced"))
+    predicted = cross_val_predict(pipe, list(entry_label_sumcmap['SUM_CMap']), entry_label_sumcmap['label'], n_jobs=-1)
+    print(classification_report(y_pred=predicted, y_true=entry_label_sumcmap['label']))
 
 
-# Parameter optimization and feature selection
-gsearch = GridSearchCV(
-    estimator=make_pipeline(
-        StandardScaler(), SelectKBest(), SVC(class_weight="balanced")
-    ),
-    param_grid={
-        "svc__C": [0.1, 1, 10],
-        "svc__gamma": ["scale", "auto", 1, 0.1, 0.01],
-        "selectkbest__k": list(range(1, 21, 3)) + ["all"],
-    },
-    n_jobs=-1,
-)
-gsearch.fit(list(entry_label_sumcmap['SUM_CMap']), entry_label_sumcmap['label'])
-print("Best score - ", gsearch.best_score_)
-print("Best-performance parameters - ", gsearch.best_params_)
+    # Parameter optimization and feature selection
+    gsearch = GridSearchCV(
+        estimator=make_pipeline(
+            StandardScaler(), SelectKBest(), SVC(class_weight="balanced")
+        ),
+        param_grid={
+            "svc__C": [0.1, 1, 10],
+            "svc__gamma": ["scale", "auto", 1, 0.1, 0.01],
+            "selectkbest__k": list(range(1, 21, 3)) + ["all"],
+        },
+        n_jobs=-1,
+    )
+    gsearch.fit(list(entry_label_sumcmap['SUM_CMap']), entry_label_sumcmap['label'])
+    print("Best score - ", gsearch.best_score_)
+    print("Best-performance parameters - ", gsearch.best_params_)
+
+    # save the best-performance model for further reproduction - https://stackoverflow.com/questions/71140633/how-to-save-the-best-estimator-in-gridsearchcv
+    model_name = 'SVM_model_cmap_' + str(args.cmap_threshold)
+    model = gsearch.best_estimator_
+    dump(model, model_name)
+    # load the model from disk
+    #model = load(model_name)
